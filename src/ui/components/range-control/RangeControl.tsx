4cb236650c529b4e5f2ffd6da030ecd948595852
Diego Sanz <diegolangos@gmail.com>
2021-01-16 10:50:03 +0100

    FEAT bigger RangeControls

--- src/ui/components/range-control/RangeControl.tsx
+++ src/ui/components/range-control/RangeControl.tsx
@@ -2,16 +2,16 @@ import styled from "styled-components";
 
 const RangeControl = styled.div`
   cursor: grabbing;
-  width: 1em;
-  height: 1em;
+  width: 1.5em;
+  height: 1.5em;
   border-radius: 100%;
   background-color: #000;
   transform: translate(-50%, 0);
   transition: width 0.25s, height 0.25s;
 
   &:hover {
-    width: 1.5em;
-    height: 1.5em;
+    width: 3em;
+    height: 3em;
   }
 `;
 
